161c1373d25fe791da91d67251f102822a20b9b6
amogh <amoghtripathi@gmail.com>
2020-09-14 04:09:45 +0530

    complete firebase with signOut func + styled google sign in button

--- src/App.js
+++ src/App.js
@@ -5,18 +5,35 @@ import HomePage from "./pages/homepage/homepage";
 import ShopPage from './pages/shop/shop';
 import Header from "./components/header/header";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
+import { auth } from "./firebase/firebase.utils";
 
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+class App extends React.Component {
+  
+  constructor(){
+    super();
 
-function App() {
+    this.state ={
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
   return (
     <div className="App">
-      <Header />
+      <Header currentUser={this.state.currentUser} />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
@@ -25,6 +42,7 @@ function App() {
     
     </div>
   );
+  }
 }
 
 export default App;
