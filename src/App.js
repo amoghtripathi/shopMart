6b05159d3eeb6ac884e1705cf88da034423b47dd
amogh <amoghtripathi@gmail.com>
2020-09-13 06:27:31 +0530

    populated shop

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/homepage/homepage";
+import ShopPage from './pages/shop/shop';
+
 
 const HatsPage = () => (
   <div>
@@ -12,10 +14,10 @@ const HatsPage = () => (
 function App() {
   return (
     <div className="App">
-      
+
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     
     </div>
