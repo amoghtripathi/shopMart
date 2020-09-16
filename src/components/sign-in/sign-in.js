7543ed13e9d1aac2b3f4cce1eb945ec75777e1f5
amogh <amoghtripathi@gmail.com>
2020-09-17 04:18:05 +0530

    added email signin functionality in sign-in component with help of auth

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./sign-in.scss";
 import FormInput from "../../components/form-input/form-input";
 import CustomButton from "../../components/custom-button/custom-button";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component{
     constructor(props){
@@ -14,9 +14,18 @@ class SignIn extends React.Component{
         };
     }
 
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
-        this.setState({email: "", password: ""});
+
+        const { email, password } = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: "", password: ""});
+        } catch(error){
+            console.log(error);
+        }
+
     };
 
     handleChange = event =>{
