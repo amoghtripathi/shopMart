9bb1cebcf2c438129d52d9c4d6d4ceadb251512c
amogh <amoghtripathi@gmail.com>
2020-09-19 06:16:23 +0530

    removed state from app.js and added reducer to app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
 import './App.css';
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from './pages/shop/shop';
 import Header from "./components/header/header";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 
 class App extends React.Component {
-  
-  constructor(){
-    super();
-
-    this.state ={
-      currentUser: null
-    };
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const { setCurrentUser } = this.props;
+    
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth =>{
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
@@ -34,7 +30,7 @@ class App extends React.Component {
           });
         });
       }
-      this.setState({currentUser: userAuth});
+      setCurrentUser(userAuth);
     });
   }
 
@@ -57,4 +53,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
