ea743e684cee666becee014a36aa1aeab46c2ed4
amogh <amoghtripathi@gmail.com>
2020-09-28 20:28:38 +0530

    checkout incomplete save

---
+++ src/pages/checkout/checkout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./checkout.scss";
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
+import {
+  selectCartItems,
+  selectCartTotal
+} from '../../redux/cart/cart.selectors';
+
