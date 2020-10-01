bb3233f16cc4cce899e9fe9e8ab0a180fb3562d2
amogh <amoghtripathi@gmail.com>
2020-10-01 22:27:10 +0530

    adding directory to state reducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -3,6 +3,8 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import userReducer from './user/user.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 import cartReducer from "./cart/cart.reducer";
 
 
@@ -14,7 +16,9 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 export default persistReducer(persistConfig, rootReducer);
