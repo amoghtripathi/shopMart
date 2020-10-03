e9001adb05f175e38eca6bf490b74090cf57b0f0
amogh <amoghtripathi@gmail.com>
2020-10-04 04:12:39 +0530

    added urlParams in shop selectors JS

--- src/pages/collection/collection.js
+++ src/pages/collection/collection.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "./collection.scss";
 import CollectionItem from "../../components/collection-item/collection-item";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
 
-
-const CollectionPage = ({ match }) =>{
-    console.log(match);
+const CollectionPage = ({ collection }) =>{
+    console.log(collection);
     return(
     <div className="collection-page">
         <h2>COLLECTION PAGE</h2>
     </div>
     )};
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps)=>({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+
+export default connect(mapStateToProps)(CollectionPage);
